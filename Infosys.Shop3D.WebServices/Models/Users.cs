d8a27b95395ecf52a1021510936ec843b093f2dc
infyprj <naturalmovies267@gmail.com>
2025-06-04 10:30:47 +0530

    Update Users.cs

--- Infosys.Shop3D.WebServices/Models/Users.cs
+++ Infosys.Shop3D.WebServices/Models/Users.cs
@@ -1,1 +1,22 @@
+namespace Infosys.Shop3D.WebServices.Models
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string Country { get; set; }
+    }
+}
 
