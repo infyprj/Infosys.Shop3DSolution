6ff2dbfcd390f3ffa62c9f335c9bf5b4c75e05ca
infyprj <naturalmovies267@gmail.com>
2025-06-04 10:30:35 +0530

    Update RatingRequest.cs

--- Infosys.Shop3D.WebServices/Models/RatingRequest.cs
+++ Infosys.Shop3D.WebServices/Models/RatingRequest.cs
@@ -1,1 +1,10 @@
+namespace Infosys.Shop3D.WebServices.Models
+{
+    public class RatingRequest
+    {
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+        public int Rating { get; set; }
+    }
+}
 
