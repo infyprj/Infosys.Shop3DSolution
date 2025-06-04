7cd432ec520ade84484cf2fe386ca09ef1cfc215
infyprj <naturalmovies267@gmail.com>
2025-06-04 10:30:22 +0530

    Update PlaceOrderRequest.cs

--- Infosys.Shop3D.WebServices/Models/PlaceOrderRequest.cs
+++ Infosys.Shop3D.WebServices/Models/PlaceOrderRequest.cs
@@ -1,1 +1,10 @@
+namespace Infosys.Shop3D.WebServices.Models
+{
+    public class PlaceOrderRequest
+    {
+        public int UserId { get; set; }
+        public int CartId { get; set; }
+        public int AddressId { get; set; }
+    }
+}
 
