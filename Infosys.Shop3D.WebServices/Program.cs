1ce1abc136719d14ac4becc5e69f3f8ed0a781dd
infyprj <naturalmovies267@gmail.com>
2025-06-04 10:31:36 +0530

    Update Program.cs

--- Infosys.Shop3D.WebServices/Program.cs
+++ Infosys.Shop3D.WebServices/Program.cs
@@ -1,1 +1,37 @@
+using Infosys.Shop3D.DataAccessLayer.Models;
+using Infosys.Shop3D.DataAccessLayer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<Shop3DContext>();
+builder.Services.AddTransient<Shop3DRepository>(
+    c => new Shop3DRepository(c.GetRequiredService<Shop3DContext>()));
+
+
+var app = builder.Build();
+
+app.UseCors(options => options.WithOrigins("*").AllowAnyMethod().AllowAnyHeader());
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
 
