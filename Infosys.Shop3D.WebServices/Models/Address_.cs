724ee0ffdd98b033c5e8481bccc4bceca1d7b910
infyprj <naturalmovies267@gmail.com>
2025-06-04 10:29:54 +0530

    Update Address_.cs

--- Infosys.Shop3D.WebServices/Models/Address_.cs
+++ Infosys.Shop3D.WebServices/Models/Address_.cs
@@ -1,1 +1,21 @@
+namespace Infosys.Shop3D.WebServices.Models
+{
+    public class Address_
+    {
+        public int AddressId { get; set; }
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string AddressLine { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string Country { get; set; }
+    }
+}
 
