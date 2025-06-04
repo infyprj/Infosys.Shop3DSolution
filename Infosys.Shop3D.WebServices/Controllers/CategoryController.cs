dcebed54ba6b2eac278109908b10adb4cbbb9655
infyprj <naturalmovies267@gmail.com>
2025-06-04 10:27:18 +0530

    Update CategoryController.cs

--- Infosys.Shop3D.WebServices/Controllers/CategoryController.cs
+++ Infosys.Shop3D.WebServices/Controllers/CategoryController.cs
@@ -1,1 +1,31 @@
+using Infosys.Shop3D.DataAccessLayer;
+using Infosys.Shop3D.DataAccessLayer.Models;
+using Microsoft.AspNetCore.Mvc;
 
+namespace Infosys.Shop3D.WebServices.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CategoryController:Controller
+    {
+        public Shop3DRepository repository;
+        public CategoryController(Shop3DRepository repository)
+        {
+            this.repository = repository;
+        }
+        [HttpGet]
+        public JsonResult GetAllCategories()
+        {
+            List<Category> categories = new List<Category>();
+            try
+            {
+                categories = repository.GetAllCategories();
+            }
+            catch (Exception ex)
+            {
+                categories = null;
+            }
+            return Json(categories);
+        }
+    }
+}
